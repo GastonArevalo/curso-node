53400a1b3765bbc1366c61f378b2b4db35c0e015
Arévalo Giordano Lucas Gastón <53384673+GastonArevalo@users.noreply.github.com>
2023-04-07 21:36:48 -0300

    create files with node

--- 01-bases-node/app.js
+++ 01-bases-node/app.js
@@ -1,3 +1,14 @@
+const fs = require('fs');
+
+let salida = "";
+const base = 5;
 for(let count = 1; count <= 10; count++){
-    console.log(`5 x ${count} = ${5 * count}`)
+    salida += `${base} x ${count} = ${base * count}\n`;
 }
+console.log(salida);
+
+fs.writeFile(`tabla-${base}.txt`, salida, (err) => {
+    if(err) throw err;
+
+    console.log(`Archivo tabla de ${base} creado`);
+})
