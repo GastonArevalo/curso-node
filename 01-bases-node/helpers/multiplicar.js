3a30905270f87a347caf83f9effdb875d97fb214
Lucas Arevalo <lucas.arevalo@accenture.com>
2023-04-10 16:13:46 -0300

    change the promise

--- 01-bases-node/helpers/multiplicar.js
+++ 01-bases-node/helpers/multiplicar.js
@@ -1,17 +1,21 @@
 const fs = require('fs');
 
 
-const createFile = async (base = 5) =>{
+const createFile = (base = 5) =>{
 
-let salida = "";
-for(let count = 1; count <= 10; count++){
-    salida += `${base} x ${count} = ${base * count}\n`;
-}
-console.log(salida);
+    return new Promise( (resolve, reject) =>{
 
-fs.writeFileSync(`tabla-${base}.txt`, salida)
+        let salida = "";
+        for(let count = 1; count <= 10; count++){
+            salida += `${base} x ${count} = ${base * count}\n`;
+        }
+        console.log(salida);
+        
+        fs.writeFileSync(`tabla-${base}.txt`, salida)
+        
+        resolve({fileName: `tabla-${base}.txt`});
 
-return {fileName: `tabla-${base}.txt`}
+    })
 
 }
 
